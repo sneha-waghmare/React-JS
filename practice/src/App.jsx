b6c3008ad98d70abe7ed745453992966fa1feac5
waghmare sneha <waghmaresnehuu@gmail.com>
2024-09-16 15:37:19 +0530

    make simple counter project

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,10 +1,42 @@
 import { useState } from 'react'
-
 function App() {
 
+let [counter,setCounter] = useState(1)
+const addValue = () => {
+  // console.log("value added",Math.random());
+
+    
+  setCounter(counter+1)
+  // console.log("Clicked",counter);
+  // counter += 1;
+  
+}
+
+const removeValue  = () => {
+
+  if(counter > 0){
+  setCounter(counter-1)
+  }
+  // console.log("remove 1 ", counter)
+  // counter -=1;
+  
+}
   return (
     <>
-      <h1>Hello world</h1>
+    <body >
+      
+         <h1>Chai or react</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button
+        onClick={addValue}
+      >Add value</button>
+      <br />
+      <button 
+        onClick={removeValue}
+      >Remove value</button>
+      </body>
+ 
     </>
   )
 }
--- practice/src/main.jsx
+++ practice/src/main.jsx
@@ -2,8 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function Sneha(){
+  return(
+  <h1>i'm sneha....</h1>
+  )
+}
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Sneha/>
   </StrictMode>,
 )
